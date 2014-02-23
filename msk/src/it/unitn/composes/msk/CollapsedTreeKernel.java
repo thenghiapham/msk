f48a1c716cf413de78583c451fff55cdd6e3cf23
thenghia.pham <thenghia.pham@unitn.it>
2014-02-23 19:20:13 +0100

    - kernel

--- msk/src/it/unitn/composes/msk/CollapsedTreeKernel.java
+++ msk/src/it/unitn/composes/msk/CollapsedTreeKernel.java
@@ -3,21 +3,41 @@ package it.unitn.composes.msk;
 import edu.berkeley.compbio.jlibsvm.kernel.KernelFunction;
 import it.uniroma2.util.tree.Tree;
 import it.uniroma2.util.vector.SemanticVectorProviderNEW;
-import it.uniroma2.util.vector.SemanticVectorProvider;
+import it.unitn.composes.composition.BasicComposition;
+import it.unitn.composes.tree.SemanticTree;
 
 public class CollapsedTreeKernel implements KernelFunction<Tree>{
 
 	private double lambda = 0.4;
-	private SemanticVectorProviderNEW semanticSpace;
+	protected SemanticVectorProviderNEW semanticSpace;
+	protected BasicComposition composition; 
+	
+	public CollapsedTreeKernel(double lambda, SemanticVectorProviderNEW semanticSpace, 
+			BasicComposition composition) {
+		this.composition = composition;
+		this.lambda = lambda;
+		this.semanticSpace = semanticSpace;
+	}
+	
+	public CollapsedTreeKernel() {
+		semanticSpace = null;
+		composition = null;
+	}
+	
 	@Override
 	public double evaluate(Tree arg0, Tree arg1) {
 		// TODO Auto-generated method stub
+		return value(arg0, arg1);
+	}
+	
+	public double value(Tree arg0, Tree arg1) {
+		SemanticTree tree1 = new SemanticTree(arg0, semanticSpace, composition);
+		SemanticTree tree2 = new SemanticTree(arg1, semanticSpace, composition);
+		return value(tree1, tree2);
+	}
+	
+	public double value(SemanticTree tree1, SemanticTree tree2) {
 		return 0;
 	}
-	
-	public CollapsedTreeKernel(double lambda, SemanticVectorProviderNEW semanticSpace) {
-		
-	}
-	
 
 }
--- msk/src/it/unitn/composes/tree/SemanticTree.java
+++ msk/src/it/unitn/composes/tree/SemanticTree.java
@@ -1,7 +1,46 @@
 package it.unitn.composes.tree;
 
+import java.util.Vector;
+
 import it.uniroma2.util.tree.Tree;
+import it.uniroma2.util.vector.SemanticVectorProviderNEW;
+import it.unitn.composes.composition.BasicComposition;
 
 public class SemanticTree extends Tree {
-	
+	private double[] vector;
+	public SemanticTree(Tree syntacticTree, SemanticVectorProviderNEW semanticSpace, BasicComposition com) {
+		String label = syntacticTree.getRootLabel();
+		setRootLabel(label);
+		Vector<Tree> children = new Vector<Tree>();
+		if (syntacticTree.isTerminal()) {
+			String posLabel = syntacticTree.getUsePosLabel();
+			this.setUsePosLabel(posLabel);
+			try {
+				vector = semanticSpace.getVector(syntacticTree.getRootLabel());
+			} catch (Exception e) {
+				vector = null;
+			}
+			
+		} else if (syntacticTree.isPreTerminal()) {
+			SemanticTree child = new SemanticTree(syntacticTree.getChildren().get(0), semanticSpace, com);
+			children.add(child);
+			
+			this.vector = child.vector;
+			this.setChildren(children);
+		} else {
+			for (Tree syntacticChild: syntacticTree.getChildren()) {
+				SemanticTree child = new SemanticTree(syntacticChild, semanticSpace, com);
+				children.add(child);
+				
+				if (child.vector != null) {
+					if (this.vector == null) {
+						this.vector = child.vector;
+					} else {
+						this.vector = com.compose(this.vector, child.vector);
+					}
+				}
+			}
+			
+		}
+	}
 }
